a371bf7b657611c4f5d795b49797b3ebdfbd01a2
Roy <toledoroy@gmail.com>
2022-03-12 14:24:14 -0800

    Added Some Networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -25,10 +25,39 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
+    //-- Test
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [`${process.env.RINKEBY_DEPLOYER_PRIV_KEY}`],
+      gas: 2100000,
+      gasPrice: 8000000000
+      // gasPrice: 10000000000,      
+    },
+	mumbai: {
+      // url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_KEY}`,
+      // url: process.env.ALCHEMY_MUMBAI_URL,
+      url: 'https://rpc-mumbai.maticvigil.com',
+      accounts: [`${process.env.MUMBAI_DEPLOYER_PRIV_KEY}`],
+      chainId: 80001,
+      // gas: 2100000,
+      // gas: 6000000,
+      // gasPrice: 1000000000,
+      // gasPrice: 8000000000, // default is 'auto' which breaks chains without the london hardfork
+      gasPrice: 10000000000,
+    },
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+	//-- Main 
+	polygon: {
+      // url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+      // url: process.env.ALCHEMY_POLYGON_URL,
+      url: "https://rpc-mainnet.maticvigil.com/",
+      accounts: [`${process.env.POLYGON_DEPLOYER_PRIV_KEY}`],
+      chainId: 137,
+      // gasPrice: 1000000000
     },
   },
   gasReporter: {
